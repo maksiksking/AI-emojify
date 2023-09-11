52be767fff057ddf796894ce810ec4270adb6f63
maksiksking <maksiksking@gmail.com>
2023-09-11 17:49:15 +0300

    Help 2

--- api/index.js
+++ api/index.js
@@ -30,6 +30,13 @@ app.get("/testGet", (req, res) => {
     res.json({ test });
 });
 
-app.listen(3030, () => {
-    console.log("Server is running on port 3030");
-});
+const port = process.env.PORT || 8080
+
+app.listen(port, (err, res) => {
+    if (err) {
+        console.log(err)
+        return res.status(500).send(err.message)
+    } else {
+        console.log('[INFO] Server Running on port:', port)
+    }
+})
