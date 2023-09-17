23b0848b6b5c4f3b4bc5af6190e7c01be22b77e9
maksiksking <maksiksking@gmail.com>
2023-09-17 23:51:00 +0300

    Vercel prod 3

--- api/index.js
+++ api/index.js
@@ -18,14 +18,7 @@
 //
 // const port = process.env.PORT || 3030
 //
-// app.listen(port, (err, res) => {
-//     if (err) {
-//         console.log(err)
-//         return res.status(500).send(err.message)
-//     } else {
-//         console.log('[INFO] Server Running on port:', port)
-//     }
-// })
+
 
 require("dotenv").config();
 const dotenv = require('dotenv');
