821f4cbf772f8800325aee00f6cdb6f2fd4b410e
maksiksking <maksiksking@gmail.com>
2023-09-17 23:39:55 +0300

    Vercel prod 1

--- api/index.js
+++ api/index.js
@@ -1,14 +1,41 @@
+// const express = require("express");
+// const app = express();
+//
+// app.use(express.static('../public'));
+//
+// ntp script (unimportant)
+// console.log("I'm alive")
+// console.log(process.env.OPENAI_API_KEY_LATEST)
+//
+//
+// app.get("/testGet", (req, res) => {
+//     // const APIkey = process.env.OPENAI_API_KEY_LATEST
+//     const test = "help"
+//     console.log(test)
+//
+//     res.json({ test });
+// });
+//
+// const port = process.env.PORT || 3030
+//
+// app.listen(port, (err, res) => {
+//     if (err) {
+//         console.log(err)
+//         return res.status(500).send(err.message)
+//     } else {
+//         console.log('[INFO] Server Running on port:', port)
+//     }
+// })
+
 require("dotenv").config();
 const dotenv = require('dotenv');
-dotenv.config({ path: '../process.env' });
-const express = require("express");
-const app = express();
 const ntpClient = require('ntp-client');
+dotenv.config({ path: '../process.env' });
+const app = require('express')();
+const { v4 } = require('uuid');
 
-app.use(express.static('../public'));
-
-// ntp script (unimportant)
-app.get("/vC4B5cmd5ybVouxbC3qnjldC", (req, res) => {
+// ntp script
+app.get("/api/ntp", (req, res) => {
     ntpClient.getNetworkTime("pool.ntp.org", 123, function(err, date) {
         if(err) {
             console.error(err);
@@ -19,25 +46,14 @@ app.get("/vC4B5cmd5ybVouxbC3qnjldC", (req, res) => {
         res.json({ date });
     });
 });
-console.log("I'm alive")
-console.log(process.env.OPENAI_API_KEY_LATEST)
 
-
-app.get("/testGet", (req, res) => {
-    // const APIkey = process.env.OPENAI_API_KEY_LATEST
-    const test = "help"
-    console.log(test)
-
-    res.json({ test });
+app.get('/api/key', (req, res) => {
+    res.json(process.env.OPENAI_API_KEY_LATEST);
 });
 
-const port = process.env.PORT || 3030
+app.get('/api/item/:slug', (req, res) => {
+    const { slug } = req.params;
+    res.end(`Item: ${slug}`);
+});
 
-app.listen(port, (err, res) => {
-    if (err) {
-        console.log(err)
-        return res.status(500).send(err.message)
-    } else {
-        console.log('[INFO] Server Running on port:', port)
-    }
-})
+module.exports = app;
